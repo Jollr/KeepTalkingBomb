a52e143eda4977bc8112ce3862d54fb177608b4a
Jollr <jolle.lont@gmail.com>
2016-04-10 21:43:49 +0200

    push button

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -1,12 +1,36 @@
+// pin numbers
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonPushCounter = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  // read the state of the pushbutton value:
+  int buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
 }
+
+/*void slumber(int ms) {
+  bool prevState = false;
+  int n = 0;
+  for (n = 0; n < ms; n++) {
+    delay(1);
+    
+  }
+}
+*/
