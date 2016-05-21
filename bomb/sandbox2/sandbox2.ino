d7edd64859df9f1c5a63a7f12dcb32d89374c9b7
Jollr <jolle.lont@gmail.com>
2016-05-21 16:12:55 +0200

    set up first prototype

--- bomb/sandbox2/sandbox2.ino
+++ bomb/sandbox2/sandbox2.ino
@@ -1,14 +1,8 @@
 // pin numbers
-const int controlPins[4] = {2, 3, 4, 5};
+const int controlPins[4] = {5, 2, 3, 4};
 const int clockPin = 6;
 const int latchPin = 7;
 const int dataPin = 8;
-
-// program state
-unsigned long prevTimeReading;
-unsigned long timeCounter = 0;
-int output = 0;
-int timeDigits[4] = {0, 0, 0, 0};
 
 void setup() {
   setPinModes();
@@ -26,34 +20,61 @@ void setPinModes() {
 }
 
 void initializeState() {
-  digitalWrite(controlPins[0], HIGH);
+  digitalWrite(controlPins[0], LOW);
   digitalWrite(controlPins[1], LOW);
   digitalWrite(controlPins[2], LOW);
   digitalWrite(controlPins[3], LOW);
-  
-  prevTimeReading = millis();
 }
 
 void loop() {
-  update7Segment();
+  for (int n = 0; n < 10000; n++) {
+    for (int m = 0; m < 15; m++) {
+      update7Segment(10000-n);
+    }
+  }
 }
 
 
-void update7Segment() {
-  writeSegment(0, 111);
-  writeSegment(1, 222);
-  writeSegment(2, 180);
-  writeSegment(3, 3);
+void update7Segment(int n) {
+  writeSegments((n / 1000) % 10, (n / 100) % 10, (n / 10) % 10, n % 10);
+}
+
+void writeSegments(int n1, int n2, int n3, int n4) {
+  writeSegment(0, get7SegmentDisplayValue(n1));
+  writeSegment(1, get7SegmentDisplayValue(n2));
+  writeSegment(2, get7SegmentDisplayValue(n3));
+  writeSegment(3, get7SegmentDisplayValue(n4));
 }
 
 void writeSegment(int segment, int value) {
   for (int n = 0; n < 4; n++) {
     digitalWrite(controlPins[n], LOW);
   }
-  
+
+  digitalWrite(latchPin, LOW);
   digitalWrite(controlPins[segment], HIGH);
   
-  digitalWrite(latchPin, LOW);
+  
   shiftOut(dataPin, clockPin, MSBFIRST, value);
   digitalWrite(latchPin, HIGH);
+  digitalWrite(controlPins[segment], LOW);
+  delay(1);
+  
 }
+
+int get7SegmentDisplayValue(int num) {
+  switch(num) {
+    case 0: return 255-(1+4+8+2+32+128); break;
+    case 1: return 255-2-32; break;
+    case 2: return 255-8-2-16-1-128; break;
+    case 3: return 255-8-2-16-32-128; break;
+    case 4: return 255-4-16-2-32; break;
+    case 5: return 255-8-4-16-32-128; break;
+    case 6: return 255-8-4-1-128-32-16; break;
+    case 7: return 255-8-2-32; break;
+    case 8: return 255-8-4-2-16-32-128-1; break;
+    case 9: return 255-4-8-2-16-32-128; break;
+    default: return 255-64; break;
+  }
+}
+
---
+++ prototype/prototype/prototype.ino
@@ -0,0 +1,29 @@
+const int potentioMeterPin = A0;
+
+// 595 controlling 7-segment
+const int latchPin = 8;
+const int dataPin = 7;
+const int clockPin = 9;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop() {
+  int val = analogRead(potentioMeterPin);  
+
+  Serial.println(val);
+  delay(200);
+}
+
+void update7Segment() {
+  digitalWrite(latchPin, LOW);
+  int numberToDisplay = 255;
+
+  shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
+  digitalWrite(latchPin, HIGH);
+}
