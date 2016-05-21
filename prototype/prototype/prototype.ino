415f4390d059074a6d51d2b727ed95160ade5b93
Jollr <jolle.lont@gmail.com>
2016-05-21 19:13:22 +0200

    set up 7-segment in prototype

--- prototype/prototype/prototype.ino
+++ prototype/prototype/prototype.ino
@@ -1,3 +1,7 @@
+//7-segment constants
+typedef enum {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, A, E} sevenSegmentValue;
+
+// input pins
 const int potentioMeterPin = A0;
 
 // 595 controlling 7-segment
@@ -17,13 +21,34 @@ void loop() {
   int val = analogRead(potentioMeterPin);  
 
   Serial.println(val);
-  delay(200);
+  update7Segment();
+  delay(10);
 }
 
 void update7Segment() {
   digitalWrite(latchPin, LOW);
-  int numberToDisplay = 255;
+  int timeStep = (millis() / 500) % 12;
+  int numberToDisplay = get7SegmentOutputValue(timeStep);
 
   shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
   digitalWrite(latchPin, HIGH);
 }
+
+int get7SegmentOutputValue(int logicalValue) {
+  switch(logicalValue) {
+    case ZERO: return 64+32+16+8+4+2; break;
+    case ONE: return 16+2; break;
+    case TWO: return 32+16+128+8+4; break;
+    case THREE: return 32+16+128+2+4; break;
+    case FOUR: return 64+128+16+2; break;
+    case FIVE: return 32+64+128+2+4; break;
+    case SIX: return 32+64+8+4+2+128; break;
+    case SEVEN: return 32+16+2; break;
+    case EIGHT: return 128+64+32+16+8+4+2; break;
+    case NINE: return 128+64+32+16+2+4; break;
+    case A: return 128+64+32+16+2+8; break;
+    case E: return 128+64+32+8+4; break;
+    default: return 0;
+  }
+}
+
