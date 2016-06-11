88f6358acfd84f2c893c7a32747ce33b93776018
Jollr <jolle.lont@gmail.com>
2016-06-11 18:56:09 +0200

    fixed serveral button input glitches

--- prototype/prototype/prototype.ino
+++ prototype/prototype/prototype.ino
@@ -1,14 +1,16 @@
 #include "pitches.h"
 
-//7-segment constants
+enum digitalTransition { SAME, LOW_TO_HIGH, HIGH_TO_LOW };
 
-// input pins
+// inputs
 const int numAnalogPins = 4;
 const int analogPins[numAnalogPins] = { A0, A1, A2, A3 };
-const int numButtonPins = 4;
+const int numButtonPins = 3;
 const int buttonPins[numButtonPins] = { 2, 3, 4 };
+digitalTransition buttonTransitions[numButtonPins] = { SAME, SAME, SAME };
 const int numSwitchPins = 3;
 const int switchPins[numSwitchPins] = { 5, 6, 7 };
+digitalTransition switchTransitions[numSwitchPins] = { SAME, SAME, SAME };
 const int numWirePins = 2;
 const int wirePins[numWirePins] = { 9, 10 };
 int analogReads[numAnalogPins];
@@ -16,28 +18,32 @@ int buttonReads[numButtonPins];
 int switchReads[numSwitchPins];
 bool wireCuts[numWirePins] = {false, false};
 
-// output pins
+// outputs
 const int speakerPin = 8;
 const int numLedValues = 8;
 int ledWrites[numLedValues] = {LOW};
 
-// 595 controlling 7-segment
+// 595 controlling output leds
 const int latchPin = 11;
 const int dataPin = 12;
 const int clockPin = 13;
 
 // bomb state
-// todo
+enum gameState { DISARMED, SEQ1, SEQ2, SEQ3, DEAD };
+gameState currentState = SEQ1;
+gameState nextState = SEQ1;
+unsigned long stateTimer = 0;
+unsigned long prevTimeStamp = millis();
 
 void setup() {
   Serial.begin(9600);
 
   for (int n = 0; n < numButtonPins; n++) {
-    pinMode(buttonPins[n], OUTPUT);
+    pinMode(buttonPins[n], INPUT);
   }
 
   for (int n = 0; n < numSwitchPins; n++) {
-    pinMode(switchPins[n], OUTPUT);
+    pinMode(switchPins[n], INPUT);
   }
   
   pinMode(latchPin, OUTPUT);
@@ -53,29 +59,203 @@ void loop() {
 }
 
 void gameLogic() {
-  //int analogReads[numAnalogPins];
-  //int buttonReads[numButtonPins];
-  //int switchReads[numSwitchPins];
-  //int ledWrites[numLedValues] = {LOW};
+  updateStateTimer();
 
-  if (wireCuts[0]) {
-    ledWrites[0] = HIGH;
-  } else {
-    ledWrites[0] = LOW;
+  switch(currentState) {
+    case DISARMED: disarmed(); break;
+    case DEAD: dead(); break;
+    case SEQ1: test(); break;
+    case SEQ2: seq2(); break;
+    case SEQ3: seq3(); break;
   }
 
-  if (wireCuts[1]) {
-    ledWrites[1] = HIGH;
-  } else {
-    ledWrites[1] = LOW;
+  stateTransition();
+}
+
+void test() {
+  return;
+}
+
+void seq1() {
+  if (buttonReads[0] == HIGH || buttonReads[2] == HIGH) {
+    nextState = DEAD;
+    return;
+  }
+ 
+  if (buttonTransitions[1] != LOW_TO_HIGH) return;
+  
+  if (analogReads[1] / 256 != 0 || analogReads[2] / 256 != 3) {
+    nextState = DEAD;
+    return;
   }
 
-  /*ledWrites[0] = buttonReads[0];
-  ledWrites[1] = buttonReads[1];
-  ledWrites[2] = buttonReads[2];
-  ledWrites[3] = switchReads[0];
-  ledWrites[4] = switchReads[1];
-  ledWrites[5] = switchReads[2];*/
+  nextState = SEQ2;
+}
+
+// simon says
+int simonStepNumber = 0;
+int simonButtonCounter = 0;
+const int simonStepCount = 6;
+const int simonSequence[simonStepCount] = { 1, 4, 1, 6, 4, 4 };
+byte previousLedPin = 0;
+
+void seq2() {
+  ledWrites[previousLedPin] = LOW;
+  int stepInAnimation = stateTimer / 300;
+  if (stepInAnimation % 2 == 0 && stepInAnimation <= 2*simonStepNumber) {
+    previousLedPin = simonSequence[stepInAnimation / 2];
+    ledWrites[previousLedPin] = HIGH;
+  }
+
+  int expectedLed = simonSequence[simonButtonCounter];
+  if ( 
+       (buttonTransitions[0] == LOW_TO_HIGH && expectedLed == 1) ||
+       (buttonTransitions[1] == LOW_TO_HIGH && expectedLed == 4) ||
+       (buttonTransitions[2] == LOW_TO_HIGH && expectedLed == 6)
+     ) {
+    
+    simonButtonCounter++;
+    
+  } else if (buttonTransitions[0] == LOW_TO_HIGH || buttonTransitions[1] == LOW_TO_HIGH || buttonTransitions[2] == LOW_TO_HIGH) {
+    nextState = DEAD;
+  }
+
+  if (simonButtonCounter > simonStepNumber) {
+    simonButtonCounter = 0;
+    simonStepNumber++;
+    stateTimer = 0;
+  }
+
+  if (simonStepNumber == simonStepCount) {
+    simonStepNumber = 0;
+    simonButtonCounter = 0;
+    previousLedPin = 0;
+    nextState = DISARMED;
+  }
+}
+
+// morse
+const int morseALength = 5;
+bool baseMorseA[morseALength] = {true, false, true, true, true};
+const int morseCLength = 12;
+bool baseMorseC[morseCLength] = {true, true, true, false, true, false, true, true, true, false, true};
+const int morseKLength = 9;
+bool baseMorseK[morseKLength] = {true, true, true, false, true, false, true, true, true };
+const int morseLLength = 10;
+bool baseMorseL[morseLLength] = {true, false, true, true, true, false, true, false, true};
+const int morseMLength = 8;
+bool baseMorseM[morseMLength] = {true, true, true, false, true, true, true};
+const int morseQLength = 17;
+bool baseMorseQ[morseQLength] = {true, true, true, false, true, true, true, false, true, false, true, true, true};
+const int morseRLength = 8;
+bool baseMorseR[morseRLength] = {true, false, true, true, true, false, true};
+
+const int morseUnitLength = 300;
+const int morseTotalLength = morseKLength + 3 + morseALength + 3 + morseRLength + 3 + morseLLength + 17;
+bool morseButtonFlag = false;
+
+void seq3() {
+nextState = DISARMED;
+  
+  int morseStep = stateTimer / morseUnitLength;
+  
+  if (   morseLetter(baseMorseK, morseKLength, 0, morseStep) 
+      || morseLetter(baseMorseA, morseALength, morseKLength + 3, morseStep)
+      || morseLetter(baseMorseR, morseRLength, morseKLength + morseALength + 2*3, morseStep)
+      || morseLetter(baseMorseL, morseLLength, morseKLength + morseALength + morseKLength + 3*3, morseStep)      
+  ) {
+    playSound();
+  }
+  else {
+    stopSound();
+  }
+
+  if (morseStep > morseTotalLength) {
+    stateTimer = 0;
+  }
+
+  if (buttonTransitions[0] == LOW_TO_HIGH || buttonTransitions[1] == LOW_TO_HIGH) {
+    Serial.println("button transitions");
+    nextState = DEAD;
+    return;
+  }
+  
+  if (buttonTransitions[2] == LOW_TO_HIGH) {
+    Serial.println("button flag on");
+    morseButtonFlag = true;
+  }
+  
+  if (!morseButtonFlag) {
+    return;
+  }
+
+  if (switchReads[0] == LOW || switchReads[2] == LOW) {
+    Serial.println("switch low");
+    nextState = DEAD;
+    return;
+  }
+
+  if (analogReads[0] < 512 || analogReads[0] > 1024 * 3 / 4) {
+    Serial.println("wrong analog value");
+    nextState = DEAD;
+    return;
+  } 
+
+  if (switchTransitions[1] == HIGH_TO_LOW) {
+    nextState = DEAD;
+    return;
+  }
+  
+  if (switchTransitions[1] == LOW_TO_HIGH) {
+    nextState = DISARMED;
+    return;
+  }
+}
+
+bool morseLetter(bool* letter, int arrayLength, int offset, int morseStep) {
+  if (morseStep >= offset && morseStep < offset + arrayLength) {
+    return letter[morseStep - offset];
+  }
+
+  return false;
+}
+
+void disarmed() {
+  int interval = 500;
+  
+  for (int n = 0; n < numLedValues; n++) {
+    ledWrites[n] = LOW;
+  }
+
+  ledWrites[(stateTimer % interval*numLedValues) / interval] = HIGH;
+
+  // todo: play sound
+}
+
+void dead() {
+  for (int n = 0; n < numLedValues; n++) {
+    ledWrites[n] = HIGH;
+  }
+
+  // todo: play sound
+}
+
+void stateTransition() {
+  if (currentState != nextState) {
+    currentState = nextState;
+    morseButtonFlag = false;
+    stateTimer = 0;
+    stopSound();
+    for (int n = 0; n < numLedValues; n++) {
+      ledWrites[n] = LOW;
+    }
+  }
+}
+
+void updateStateTimer() {
+  unsigned long timeStamp = millis();
+  stateTimer = stateTimer + (timeStamp - prevTimeStamp);
+  prevTimeStamp = timeStamp;
 }
 
 void readInputs() {
@@ -95,13 +275,53 @@ void readAnalogInputs() {
   }
 }
 
-void readDigitalInputs() {
+void readDigitalInputs() { 
+  
   for (int n = 0; n < numButtonPins; n++) {
-    buttonReads[n] = digitalRead(buttonPins[n]);
+    int newRead = digitalRead(buttonPins[n]);
+      
+    if (newRead != buttonReads[n]) {
+      if (newRead == HIGH) {
+        Serial.print("button ");
+        Serial.print(n);
+        Serial.println(" low to high");
+      }
+      else {
+        buttonTransitions[n] = HIGH_TO_LOW;
+        Serial.print("button ");
+        Serial.print(n);
+        Serial.println(" high to low");
+      }
+    }
+    else {
+      buttonTransitions[n] = SAME;
+    }
+    
+    buttonReads[n] = newRead;
   }
 
   for (int n = 0; n < numSwitchPins; n++) {
-    switchReads[n] = digitalRead(switchPins[n]);
+    int newRead = digitalRead(switchPins[n]);
+
+    if (newRead != switchPins[n]) {
+      if (newRead == HIGH) {
+        switchTransitions[n] = LOW_TO_HIGH;
+        Serial.print("switch ");
+        Serial.print(n);
+        Serial.println(" low to high");
+      }
+      else {
+        switchTransitions[n] = HIGH_TO_LOW;
+        Serial.print("switch ");
+        Serial.print(n);
+        Serial.println(" high to low");
+      }
+    }
+    else {
+      switchTransitions[n] = SAME;
+    }
+    
+    switchReads[n] = newRead;
   }
 }
 
@@ -113,6 +333,7 @@ void readWireCuts() {
 
 void writeOutputs() {
   writeLeds();
+  controlSound();
 }
 
 void writeLeds() {
@@ -134,11 +355,33 @@ void writeTo595(int value) {
   digitalWrite(latchPin, HIGH);
 }
 
- void sound() {
-  if ( (millis() / 500) % 2 == 0 ) {
-    tone(speakerPin, NOTE_C2, 20);
+unsigned long soundStartTime = 0;
+int soundPeriod = 0;
+void controlSound() {
+  if (soundPeriod == 0) return;
+
+  unsigned long now = micros();
+  if ( ((now - soundStartTime) / soundPeriod) % 2 == 0) {
+    digitalWrite(speakerPin, HIGH);
+  } else {
+    digitalWrite(speakerPin, LOW);
   }
- }
+}
+
+bool playingSound = false;
+void playSound() {
+  if (playingSound) return;
+
+  playingSound = true;
+  soundStartTime = micros();
+  soundPeriod = 1000000 / NOTE_G3; // microseconds
+}
+
+void stopSound() {
+  playingSound = false;
+  soundPeriod = 0;
+  digitalWrite(speakerPin, LOW);
+}
 
 /* void writeToLeds(ledMode mode) {
   digitalWrite(yellowLedPin, LOW);
